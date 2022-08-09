bfc7615120dc3a0689232c4f05d6c5632d8ad3fc
Sayyed Mohsen Zahraee <zahraee.sm@gmail.com>
2022-08-09 17:30:10 +0430

    okay http sever connecton

--- TestServer/main.cpp
+++ TestServer/main.cpp
@@ -1,9 +1,99 @@
+//
+//  sample.cc
+//
+//  Copyright (c) 2019 Yuji Hirose. All rights reserved.
+//  MIT License
+//
+
+#include <chrono>
+#include <cstdio>
+#include <httplib.h>
+
+#define SERVER_CERT_FILE "./cert.pem"
+#define SERVER_PRIVATE_KEY_FILE "./key.pem"
+
+using namespace httplib;
+
+std::string dump_headers(const Headers &headers) {
+  std::string s;
+  char buf[BUFSIZ];
+
+  for (auto it = headers.begin(); it != headers.end(); ++it) {
+    const auto &x = *it;
+    snprintf(buf, sizeof(buf), "%s: %s\n", x.first.c_str(), x.second.c_str());
+    s += buf;
+  }
+
+  return s;
+}
+
+std::string log(const Request &req, const Response &res) {
+  std::string s;
+  char buf[BUFSIZ];
+
+  s += "================================\n";
+
+  snprintf(buf, sizeof(buf), "%s %s %s", req.method.c_str(),
+           req.version.c_str(), req.path.c_str());
+  s += buf;
+
+  std::string query;
+  for (auto it = req.params.begin(); it != req.params.end(); ++it) {
+    const auto &x = *it;
+    snprintf(buf, sizeof(buf), "%c%s=%s",
+             (it == req.params.begin()) ? '?' : '&', x.first.c_str(),
+             x.second.c_str());
+    query += buf;
+  }
+  snprintf(buf, sizeof(buf), "%s\n", query.c_str());
+  s += buf;
+
+  s += dump_headers(req.headers);
+
+  s += "--------------------------------\n";
+
+  snprintf(buf, sizeof(buf), "%d %s\n", res.status, res.version.c_str());
+  s += buf;
+  s += dump_headers(res.headers);
+  s += "\n";
+
+  if (!res.body.empty()) { s += res.body; }
+
+  s += "\n";
+
+  return s;
+}
+
+int main(void) {
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+  SSLServer svr(SERVER_CERT_FILE, SERVER_PRIVATE_KEY_FILE);
+#else
+  Server svr;
+#endif
+
+  if (!svr.is_valid()) {
+    printf("server has an error...\n");
+    return -1;
+  }
+
+  svr.Post("/notify", [=](const Request & req, Response &res) {
+   printf("server got json...\n");
+   printf("body is: %s \n",req.body.c_str());
+  });
 
 
-#include "https_test_server.h"
+  svr.set_error_handler([](const Request & /*req*/, Response &res) {
+    const char *fmt = "<p>Error Status: <span style='color:red;'>%d</span></p>";
+    char buf[BUFSIZ];
+    snprintf(buf, sizeof(buf), fmt, res.status);
+    res.set_content(buf, "text/html");
+  });
 
-int main(){
+  svr.set_logger([](const Request &req, const Response &res) {
+    printf("%s", log(req, res).c_str());
+  });
 
-    HttpServer::init();
-    return 0;
+  svr.listen("localhost", 8083);
+
+  return 0;
 }
