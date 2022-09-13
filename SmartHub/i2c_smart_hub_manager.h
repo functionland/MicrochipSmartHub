10e094118605c901b3ae77ed2aafa2711bdef95e
smzahraee <zahraee.sm@gmail.com>
2022-09-14 00:22:14 +0430

    support i2c version of smart hub

---
+++ SmartHub/i2c_smart_hub_manager.h
@@ -0,0 +1,10 @@
+
+#ifndef USB_SMART_HUB_MANAGER_H
+#define USB_SMART_HUB_MANAGER_H
+
+class I2CSmartHubManager {
+public :
+    I2CSmartHubManager();
+    ~I2CSmartHubManager()=default;
+};
+#endif // USB_SMART_HUB_MANAGER_H
---
+++ SmartHub/usb_smart_hub_manager.h
@@ -0,0 +1,37 @@
+
+
+#ifndef USB_SMART_HUB_MANAGER_H
+#define USB_SMART_HUB_MANAGER_H
+
+#include <libusb-1.0/libusb.h>
+#include <vector>
+#include <array>
+class UsbSmartHubManager
+{
+public:
+    UsbSmartHubManager();
+    ~UsbSmartHubManager();
+    void SetVidPid(uint16_t vid,uint16_t pid);
+
+    bool Initialize();
+
+    bool RegisterRead(uint32_t address,uint16_t length,std::vector<uint8_t> &buffer);
+    bool RegisterWrite(uint32_t address,uint16_t length,const std::vector<uint8_t> &buffer);
+
+    bool PortMappingUsb2(std::array<uint8_t,7> port_map);
+    bool PortMappingUsb3(std::array<uint8_t,7> port_map);
+
+    bool CloseEverything();
+
+private:
+    // libusb variables
+    libusb_device_handle *handle_ = NULL;
+    // Context Variable used for initializing LibUSB session
+    libusb_context *ctx_ = NULL;
+
+    uint16_t vid_=0x0424;
+    uint16_t pid_=0x7240;
+    int last_error_code=0;
+};
+
+#endif // end USB_SMART_HUB_MANAGER_H
